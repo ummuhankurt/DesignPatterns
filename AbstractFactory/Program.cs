82bdefaae13c602b6808c364152047ba0fa4e7d8
Ümmühan Kurt <63405463+ummuhankurt@users.noreply.github.com>
2023-10-05 22:37:49 +0300

    Factory Method Pattern and Abstract Factory  Pattern.

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,102 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        ProductManager productManager = new ProductManager(new Factory1());
+        productManager.GetAll();
+    }
+
+
+    public abstract class Logging
+    {
+        public abstract void Log(string message);
+    }
+
+    public abstract class Caching
+    {
+        public abstract void Cache(string data);
+    }
+
+    public class Log4NetLogger : Logging
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine("Logged with Log4Net");
+        }
+    }
+
+    public class NLogger : Logging
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine("Logged with NLogger");
+        }
+    }
+
+   
+
+    public class MemCache : Caching
+    {
+        public override void Cache(string data) 
+        {
+            Console.WriteLine("Cach with Memcache");
+        }
+    }
+
+    public class RedisCache : Caching
+    {
+        public override void Cache(string data)
+        {
+            Console.WriteLine("Cach with Rediscache");
+        }
+    }
+
+    public abstract class CrossCottingConcernsFactory
+    {
+        public abstract Logging CreateLogger();
+        public abstract Caching CreateCaching();
+    }
+
+    public class Factory1 : CrossCottingConcernsFactory
+    {
+        public override Caching CreateCaching()
+        {
+            return new MemCache();
+        }
+
+        public override Logging CreateLogger()
+        {
+            return new Log4NetLogger();
+        }
+    }
+
+    public class Factory2 : CrossCottingConcernsFactory
+    {
+        public override Caching CreateCaching()
+        {
+            return new RedisCache();
+        }
+
+        public override Logging CreateLogger()
+        {
+            return new NLogger();
+        }
+    }
+
+    public class ProductManager
+    {
+        CrossCottingConcernsFactory _crossCottingConcernsFactory;
+        Logging _logging;
+        Caching _caching;
+        public ProductManager(CrossCottingConcernsFactory crossCottingConcernsFactory)
+        {
+            _crossCottingConcernsFactory = crossCottingConcernsFactory;
+        }
+        public void GetAll()
+        {
+            _caching.Cache("data");
+            _logging.Log("Message");
+            Console.WriteLine("Product listed.");
+        }
+    }
+}
---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,69 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Bu fabrika sonucunda hangi logger üretilecekse o logger üretilecek.
+        CustomerManager customerManager = new CustomerManager(new LoggerFactory2());
+        customerManager.Save();
+    }
+}
+
+public class LoggerFactory : ILogerFactory
+{
+    // Asıl iş burada gerçekleşir. Örn webconfiğe bakılıp nasıl loglanıyorsa o döndürülür.
+    // Factory to decide on business
+    public ILogger CreateLogger()
+    {
+        return new EdLogger();
+    }
+}
+
+public class LoggerFactory2 : ILogerFactory
+{
+    public ILogger CreateLogger()
+    {
+        return new Log4NetLogger();
+    }
+}
+
+public interface ILogger
+{
+    void Log();
+}
+
+public interface ILogerFactory
+{
+    public ILogger CreateLogger();
+}
+
+public class EdLogger : ILogger
+{
+    public void Log()
+    {
+        Console.WriteLine("Logged with EdLogger");
+    }
+}
+
+public class Log4NetLogger : ILogger
+{
+    public void Log()
+    {
+        Console.WriteLine("Logged with Log4NetLogger.");
+    }
+}
+
+
+public class CustomerManager
+{
+    private ILogerFactory _loggerFactory;
+    public CustomerManager(ILogerFactory loggerFactory)
+    {
+        _loggerFactory = loggerFactory;
+    }
+    public void Save()
+    {
+        Console.WriteLine("Saved.");
+        //ILogger logger = new EdLogger(); Böyle yapmyoruz.Böyle yaparsak bağımlı oluruz.
+        _loggerFactory.CreateLogger().Log();
+    }
+}
